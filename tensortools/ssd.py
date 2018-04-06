4c5bf7612e1cb854186c2c4fb8e78bfc3851c9e1
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-04-06 15:04:42 -0300

    init ssd, stats files

--- tensortools/ssd.py
+++ tensortools/ssd.py
@@ -1,14 +1,6 @@
-import random
-
-import numpy as np
-
-from tensortools import utils
-from tensortools.stats import k_means
-
-
 def generate_anchors(annotations, num_clusters):
     annotations = np.array(annotations)
 
     indices = [random.randrange(len(annotations)) for _ in range(num_clusters)]
     initial_centroids = annotations[indices]
-    return k_means(annotations, initial_centroids)
+    return _k_means(annotations, initial_centroids)

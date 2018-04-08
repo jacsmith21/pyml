f1ce1a4917d3db45bfaa793c550c25181554ae9c
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-04-08 10:16:06 -0300

    ready for 0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='tensortools',
     packages=['tensortools'],
-    version='0.1',
+    version='0.2',
     description='A python utility package for Machine Learning stuff.',
     author='Jacob Smith',
     author_email='jacob.smith@unb.ca',
     url='http://github.com/jacsmith21/tensortools',
-    download_url='https://github.com/jacsmith21/tensortools/archive/0.1.tar.gz',
+    download_url='https://github.com/jacsmith21/tensortools/archive/0.2.tar.gz',
     keywords=['python', 'ml', 'machine', 'learning', 'yolo'],
     classifiers=[],
 )

665dde8813424ee2a41e1330d720871be2e65755
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-04-09 13:04:34 -0300

    added lazy loading for files which use tensorflow

--- tensortools/__init__.py
+++ tensortools/__init__.py
@@ -1,4 +1,10 @@
-from tensortools import ops
 from tensortools import image
-from tensortools import hooks
 from tensortools import utils
+from tensortools import ssd
+from tensortools import yolo
+from tensortools import abc
+from tensortools import generator
+
+hooks = abc.LazyLoader('hooks', globals(), 'tensortools.hooks')
+ops = abc.LazyLoader('ops', globals(), 'tensortools.ops')
+del LazyLoader

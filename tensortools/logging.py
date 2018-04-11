e635b197ff80bda82bc7c1bf2e1b3c445e3112b8
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-04-11 11:37:24 -0300

    0.2.3 release

--- tensortools/logging.py
+++ tensortools/logging.py
@@ -1,8 +1,15 @@
 import logging
-import sys
-
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+import time
 
 
-def get_logger(name):
-    return logging.getLogger(name)
+def get_logger(name, level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    console_handler = logging.StreamHandler()
+    formatter = logging.Formatter('[%(levelname)s/%(asctime)s/%(module)s:%(lineno)s] - %(message)s')
+    formatter.converter = time.gmtime
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    return logger
